d66573379cc5f2bca189fbd9e67b84be790b4932
christinebeaubrun <christinebeaubrun@gmail.com>
2014-10-16 16:15:48 -0700

    Grunt has uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,34 @@ module.exports= function(grunt) {
   // ALL CONFIGURATION GOES HERE
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
     concat: {
-      // CONFIGURATION FOR CONCATINATING FILES GOES HERE
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src:['public/js/global.js','public/js/bootstrap.js','public/js/prefixfree.js'],
+        dest: 'dist/build.js',
+      },
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'dest/output.min.js': ['dist/build.js']
+        }
+      }
     }
-  });
+});
+
   // WHERE WE TELL GRUNT WE PLAN TO USE THIS PLUG IN
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   /* WHERE WE TELL GRUNT WHAT TO DO WHEN WE TYPE 'GRUNT'
   INTO THE TERMINAL */
-  grunt.registerTask('default', ['concat']);
+  grunt.registerTask('default', ['concat','uglify']);
 };
