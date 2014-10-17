29169c5c8f129a1d49c2e008490d669442db63c5
christinebeaubrun <christinebeaubrun@gmail.com>
2014-10-16 17:25:28 -0700

    Grunt jade added

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports= function(grunt) {
       },
       dist: {
         src:['public/js/global.js','public/js/bootstrap.js','public/js/prefixfree.js'],
-        dest: 'dist/build.js',
+        dest: 'dest/js/build.js',
       },
     },
 
@@ -20,17 +20,54 @@ module.exports= function(grunt) {
       },
       my_target: {
         files: {
-          'dest/output.min.js': ['dist/build.js']
-        }
-      }
-    }
+          'dest/js/output.min.js': ['dest/js/build.js']
+        },
+      },
+    },
+
+    imagemin: {
+      dynamic: {
+          files: [{
+              expand: true,
+              cwd: 'public/images/',
+              src: ['*.{png,jpg,gif}'],
+              dest: 'dest/images/build/'
+          }]
+      },
+    },
+
+    watch: {
+      scripts: {
+        files: ['public/js/*.js'],
+        tasks: ['concat','uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+    },
+
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          }
+        },
+        files: {
+          "dest/dest.html": ["views/index.jade"]
+        },
+      },
+    },
 });
 
   // WHERE WE TELL GRUNT WE PLAN TO USE THIS PLUG IN
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jade');
 
   /* WHERE WE TELL GRUNT WHAT TO DO WHEN WE TYPE 'GRUNT'
   INTO THE TERMINAL */
-  grunt.registerTask('default', ['concat','uglify']);
+  grunt.registerTask('default', ['concat','uglify', 'imagemin', 'jade']);
 };
